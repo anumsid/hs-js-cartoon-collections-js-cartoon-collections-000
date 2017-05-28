3a8a04b89e087874cae1f9c822cfa440b6d6f9cd
Anum Siddiqui <asiddiqui0692@gmail.com>
2017-05-28 23:01:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 function dwarfRollCall(dwarves) {
+var emptyArray=[]
+for (var i=0;i<dwarves.length;i++){
+var string=`${i+1}. ${dwarves[i]}`
+emptyArray.push(string)
+}
+return emptyArray.join(` `)
 }
 
+
 function summonCaptainPlanet(planeteerCalls){
-}
+  var emptyArray=[]
+  for (var i=0;i<planeteerCalls.length;i++){
+  var string=planeteerCalls[i].toUpperCase() + "!";
+  emptyArray.push(string)
+  }
+  return emptyArray
+  }
 
 function longPlaneteerCalls(words) {
 }
